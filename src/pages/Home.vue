9166695be5821ed75e1aea7c376b8815910282d8
timalsina-yuvraj <timalsinayuvraj@gmail.com>
2022-09-09 11:09:24 +0545

    Qweet List

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,6 @@
 <template>
-  <q-page padding>
+  <q-page>
+    <!-- qweet form -->
     <div class="row q-py-md q-px-sm items-end q-col-gutter-md">
       <div class="col">
         <q-input
@@ -13,7 +14,8 @@
         >
           <template v-slot:before>
             <q-avatar size="xl">
-              <q-img src="https://cdn.quasar.dev/img/avatar5.jpg"/>
+              <q-img
+                src="https://media-exp1.licdn.com/dms/image/C4E03AQF3aIRxq4BlBw/profile-displayphoto-shrink_100_100/0/1599398358509?e=1668038400&v=beta&t=TBvwQjR_MjdYI9Cz85N_7dPI4Ux0hUDfX5Uvw9YWt44"/>
             </q-avatar>
           </template>
         </q-input>
@@ -30,6 +32,47 @@
       </div>
     </div>
     <q-separator class="divider" color="grey-2" size="10px"/>
+
+    <!-- qweet list -->
+    <q-list>
+
+      <q-item clickableq-py-md>
+        <q-item-section avatar top>
+          <q-avatar>
+            <q-img src="https://cdn.quasar.dev/img/avatar2.jpg"/>
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-subtitle1">
+            <strong>
+              Lorem Bdr. Ipsum
+            </strong>
+            <span class="text-grey-7">
+              @lorem_ipsum
+            </span>
+          </q-item-label>
+          <q-item-label class="qweet-content text-body1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+
+            A consequatur cupiditate doloremque eaque eius
+            enim, et eveniet, excepturi explicabo fuga, labore
+            libero officiis optio tempora totam? Cupiditate earum id.
+          </q-item-label>
+          <div class="row qweet-icons justify-between q-mt-sm">
+            <q-btn color="grey" flat icon="far fa-comment" round size="sm"/>
+            <q-btn color="grey" flat icon="fas fa-retweet" round size="sm"/>
+            <q-btn color="grey" flat icon="far fa-heart" round size="sm"/>
+            <q-btn color="grey" flat icon="fas fa-trash" round size="sm"/>
+          </div>
+        </q-item-section>
+
+        <q-item-section side top>
+          1 min ago
+        </q-item-section>
+      </q-item>
+
+    </q-list>
   </q-page>
 </template>
 
@@ -44,8 +87,15 @@ const newQweetContent = ref("");
     font-size: 19px
     line-height: 1.4 !important
     overflow: hidden
+
 .divider
   border-top: 1px solid
   border-bottom: 1px solid
   border-color: $grey-4
+
+.qweet-content
+  white-space: pre-line
+
+.qweet-icons
+  margin-left: -5px
 </style>
